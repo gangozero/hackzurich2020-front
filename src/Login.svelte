2b364cf567ecd8bc1854e821726ef802620622f3
AlexxNB <iam@alexxnb.ru>
2020-09-19 16:25:49 +0300

    fix

--- src/Login.svelte
+++ src/Login.svelte
@@ -1,10 +1,11 @@
 <script>
     import {Input,Typography,Button,Block} from 'svelte-atoms';
 
-    const form = {
-        email:'',
-        password: ''
-    }
+    let email = '';
+    let password = '';
+
+    let errEmail = null;
+    let errPassword = null;
 
     let loading = false;
 
@@ -15,8 +16,8 @@
 
 <Block type="block1">
     <Typography type="title" block>Please login</Typography>
-    <p><Input title="E-Mail" bind:value={form.email} size="big" /></p>
-    <p><Input title="Password" bind:value={form.password} size="big" type="password" /></p>
+    <p><Input title="E-Mail" bind:value={email} size="big" error={errEmail}/></p>
+    <p><Input title="Password" bind:value={password} size="big" type="password"  error={errPassword}/></p>
 
     <p class="text-center">
         <Button type="flat" iconRight="chevron-right" isLoading={loading} on:click={doLogin}>
