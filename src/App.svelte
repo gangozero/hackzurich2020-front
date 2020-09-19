91e4df325ea03e4c97ec648238f4f2605bfee920
AlexxNB <iam@alexxnb.ru>
2020-09-20 01:04:14 +0300

    fix width

--- src/App.svelte
+++ src/App.svelte
@@ -8,16 +8,18 @@
 	import Match from './match/Match.svelte';
 
 	router.useHashNavigation();
-	user.set('159c7e6e-9131-4606-969e-046e44e879df');
+	//user.set('159c7e6e-9131-4606-969e-046e44e879df');
 	$: if(!$user) router.goto('/login');
 </script>
 
 <Variables />
 
+<div style="max-width: 500px; margin-left: auto; margin-right: auto">
 <Container>
 		<Route path="/login"><Login/></Route>
 		<Route path="/cabinet"><Cabinet/></Route>
 		<Route path="/match/:id"><Match/></Route>
 </Container>
+</div>
 
 <Notifications />
