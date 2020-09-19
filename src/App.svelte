31bd0417172e77f970ba7274bf32057164ff08eb
AlexxNB <iam@alexxnb.ru>
2020-09-19 16:21:17 +0300

    login form

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
 <script>
-	export let name;
+	import {Route,router} from 'tinro';
+	import {Variables,Container} from "svelte-atoms";
+	import {user} from './lib/userStore.js'
+	import Login from './Login.svelte';
+
+	router.useHashNavigation();
+	$: if(!$user) router.goto('/login');
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Variables />
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Container>
+	<Route path="/login"><Login/></Route>
+</Container>
---
+++ src/Login.svelte
@@ -0,0 +1,26 @@
+<script>
+    import {Input,Typography,Button,Block} from 'svelte-atoms';
+
+    const form = {
+        email:'',
+        password: ''
+    }
+
+    let loading = false;
+
+    function doLogin(){
+        loading = true;
+    }
+</script>
+
+<Block type="block1">
+    <Typography type="title" block>Please login</Typography>
+    <p><Input title="E-Mail" bind:value={form.email} size="big" /></p>
+    <p><Input title="Password" bind:value={form.password} size="big" type="password" /></p>
+
+    <p class="text-center">
+        <Button type="flat" iconRight="chevron-right" isLoading={loading} on:click={doLogin}>
+            Sign in
+        </Button>
+    </p>
+</Block>
